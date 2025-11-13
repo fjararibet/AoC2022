04644d0f98996da255bf2dc0dc1ae41aa02cde94
Felipe Jara <fjararibet@gmail.com>
2025-11-13 18:28:09 -0300

    day 5

--- d7/p2.py
+++ d7/p2.py
@@ -32,13 +32,14 @@ class Dir():
             size += c.get_size()
         return size
 
-    def get_ans(self):
+    def get_ans(self, total_size):
         global ans
         size = self.get_size()
-        if size < 100000:
-            ans += size
+        unused_space = 70000000 - total_size
+        if unused_space + size > 30000000:
+            ans = min(size, ans) if ans else size
         for c in self.children:
-            c.get_ans()
+            c.get_ans(total_size=total_size)
 
     def __str__(self):
         return self._str(0)
@@ -76,5 +77,5 @@ for line in input.split("\n")[1:]:
         curr.files.append(File(name=name, size=size))
 
 
-root.get_ans()
+root.get_ans(total_size=root.get_size())
 print(ans)
